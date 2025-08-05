93790d22b310c37a3560a0b9756de4c7b0373970
Elia Giolli <eliagiolli22@gmail.com>
2025-08-05 18:17:12 +0200

    refactor with OOP of InfoLogService.ts

--- src/services/infoLogService.ts
+++ src/services/infoLogService.ts
@@ -1,55 +1,36 @@
 import { type InfoLog } from "../types/infoLog.js";
 import * as fs from 'fs/promises';
 
-export async function readLogs(){
+//Logger service with OOP
+export class LoggerService {
 
-    try{
-        const data = await fs.readFile('../data/info.log', 'utf-8');
+    constructor(private readonly filePath:string){}
+
+    async readLogs(){
+
+        const data = await fs.readFile(this.filePath, 'utf-8');
         const log = JSON.parse(data);
 
         return log;
-    }catch(err){
-        //If the file doesn't exist, return an empty array
-        if((err as NodeJS.ErrnoException).code === 'ENOENT'){
-            return [];
-        }
-        console.error('unable to read the file', err);
-        throw err;
     }
-}
+    async writeLogs(newLog:InfoLog){
 
-export async function writeLogs(newLog:InfoLog){
-    
-    try{
-        const logs = await readLogs() ?? [];
+        const logs = await this.readLogs() ?? [];
 
         logs.push(newLog);
 
-        await fs.writeFile('../data/info.log', JSON.stringify(logs, null, 2), 'utf-8');
-
-        return newLog;
-    }catch(err){
-
-        console.error('impossible to write a new log', err);
-        throw err;
+        await fs.writeFile(this.filePath, JSON.stringify(logs, null, 2), 'utf-8');
     }
 
-}
-
-export async function deleteLogById(id:string){
-
-    try{
-        const logs = await readLogs();
+    async deleteLogById(id:string){
+        const logs = await this.readLogs();
         const updatedLogs = logs.filter((log: InfoLog) => log.id !== id);
 
         if (logs.length === updatedLogs.length) {
             console.warn('No log found with the provided ID');
         }
 
-        await fs.writeFile('../data/info.log', JSON.stringify(updatedLogs, null, 2), 'utf-8');
+        await fs.writeFile(this.filePath, JSON.stringify(updatedLogs, null, 2), 'utf-8');
         return updatedLogs;
-    }catch(err){
-        console.error('Unable to delete the log', err);
-        throw err;
     }
 }
